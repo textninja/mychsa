1ce2b09383dcc525c3f724ed79296f549bad3a76
Joe Taylor <joe@textninja.net>
2021-03-22 03:20:17 -0700

    Updated run helper

--- e2e/run_e2e.sh
+++ e2e/run_e2e.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t e2e ./e2e && \
-docker run -e MYCHSA_E2E_TARGET="http://frontend:80" --network mychsa_default --sysctl net.ipv6.conf.all.disable_ipv6=1 --rm -it e2e npx mocha
+docker run -e MYCHSA_E2E_TARGET="http://frontend:80" --network mychsa_default --sysctl net.ipv6.conf.all.disable_ipv6=1 --rm e2e npx mocha
